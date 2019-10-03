6c552dedc014f0f92e2a0d1ab982c9f9547ba32e
Scott Jordan <scottjordanswimming@gmail.com>
2019-10-03 20:30:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
-# Add your code here
+class Dog 
+  
+  @@all = []
+  
+  def initialize
+    @name = name
+    @@all << self
+  end
+  
+  def name
+    @name = name
+  end
+  
+ def self.print_all
+  @@all
+end
+
+end
+
